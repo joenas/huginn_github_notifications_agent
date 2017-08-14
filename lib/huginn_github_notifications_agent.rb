58655add9cf45cd4f631a70b3f32a439fd7da9bb
joenas <jon@jonnev.se>
2017-08-14 13:39:37 +0200

    First commit :)

---
+++ lib/huginn_github_notifications_agent.rb
@@ -0,0 +1,4 @@
+require 'huginn_agent'
+
+#HuginnAgent.load 'huginn_github_notifications_agent/concerns/my_agent_concern'
+HuginnAgent.register 'huginn_github_notifications_agent/github_notifications_agent'
